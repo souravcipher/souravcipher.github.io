804acf8de88b4953e31fa9419c0b8e30e73124d9
Sourav Das <souravcipher@gmail.com>
2026-06-15 17:09:46 +0530

    add all posts

--- _posts/2015-10-20-disqus-comments.md
+++ _posts/2015-10-20-disqus-comments.md
@@ -1,12 +1,12 @@
 ---
 layout: post
-title: test title
+title: a post with disqus comments
 date: 2015-10-20 11:59:00-0400
-description: test description
+description: an example of a blog post with disqus comments
 tags: comments
 categories: sample-posts external-services
 disqus_comments: true
 related_posts: false
 ---
 
-This post is a test blog
+This post shows how to add DISQUS comments.
--- test/integration_comments.sh
+++ test/integration_comments.sh
@@ -20,6 +20,16 @@ YAML
 
 bundle exec jekyll build --config "_config.yml,${tmp_override}" -d "${tmp_site}" >/dev/null
 
+giscus_page="${tmp_site}/blog/2022/giscus-comments/index.html"
+disqus_page="${tmp_site}/blog/2015/disqus-comments/index.html"
 
+grep -q 'https://giscus.app/client.js' "${giscus_page}"
+if grep -q 'giscus comments misconfigured' "${giscus_page}"; then
+  echo "unexpected giscus misconfiguration warning in ${giscus_page}" >&2
+  exit 1
+fi
+
+grep -q 'id="disqus_thread"' "${disqus_page}"
+grep -q '.disqus.com/embed.js' "${disqus_page}"
 
 echo "comments integration checks passed"
